0ab2dca401e965e940570c9e032a0524b331624d
Frank Hunleth <fhunleth@troodon-software.com>
2012-09-20 10:53:35 -0700

    ubl: Add configuration target name

--- boot/ubl/ubl.mk
+++ boot/ubl/ubl.mk
@@ -8,15 +8,14 @@ UBL_SITE = git@bitbucket.org:fhunleth/dm36x-flash-utils.git
 UBL_SITE_METHOD = git
 
 UBL_INSTALL_IMAGES = YES
-
-UBL_BUILD_TYPE = dm368sdmmc
+UBL_CONFIGNAME = $(call qstrip,$(BR2_TARGET_UBL_CONFIGNAME))
 
 define UBL_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/DM36x/GNU/ubl/build TYPE=$(UBL_BUILD_TYPE)
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)/DM36x/GNU/ubl/build TYPE=$(UBL_CONFIGNAME)
 endef
 
 define UBL_INSTALL_IMAGES_CMDS
-	cp -dpf $(@D)/DM36x/GNU/ubl/ubl_DM36x_$(UBL_BUILD_TYPE).bin $(BINARIES_DIR)/
+	cp -dpf $(@D)/DM36x/GNU/ubl/ubl_DM36x_$(UBL_CONFIGNAME).bin $(BINARIES_DIR)/
 endef
 
 $(eval $(generic-package))
