9dd05e4ca164039f29fcbe1147e2707672851cb4
Frank Hunleth <fhunleth@troodon-software.com>
2012-09-20 10:54:05 -0700

    Add ubl configuration target to choose dm365 sd card loader

--- build.sh
+++ build.sh
@@ -2,19 +2,22 @@
 
 set -e
 
+DEFCONFIG=leopardboard_dm368_defconfig
 VERSION=`git describe --dirty`
 
 FW_FILENAME=leopardboard_$VERSION.fw
-echo Building $FW_FILENAME...
+echo Config: $DEFCONFIG
+echo Output: $FW_FILENAME...
 echo ------------------------------
 
 # Qt things won't compile right if QMAKESPEC is set.
 unset QMAKESPEC
 
-make leopardboard_dm368_defconfig
+make $DEFCONFIG
 make
 
-output/host/usr/bin/packager -v $VERSION output/images/ubl_DM36x_dm368sdmmc.bin output/images/u-boot.bin output/images/rootfs.ext2 $FW_FILENAME
+UBLNAME=`grep UBL_CONFIGNAME .config | awk -F\" '{print $(NF-1)}'`
+output/host/usr/bin/packager -v $VERSION output/images/ubl_DM36x_$UBLNAME.bin output/images/u-boot.bin output/images/rootfs.ext2 $FW_FILENAME
 
 echo ------------------------------
 echo Built $FW_FILENAME
