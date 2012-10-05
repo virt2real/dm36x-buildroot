b931c427bba687c45ea9ac74790c37e34dc689d8
Frank Hunleth <fhunleth@troodon-software.com>
2012-10-04 22:34:15 -0400

    Add dropbear; Enable camera in kernel; Fix SDCard length

--- package/dm36x-packager/dm36x-packager.mk
+++ package/dm36x-packager/dm36x-packager.mk
@@ -3,7 +3,7 @@
 # dm36x-packager
 #
 #############################################################
-DM36X_PACKAGER_VERSION = 277df03
+DM36X_PACKAGER_VERSION = cdd5415
 DM36X_PACKAGER_SITE = git@bitbucket.org:fhunleth/dm36x-packager.git
 DM36X_PACKAGER_SITE_METHOD = git
 
