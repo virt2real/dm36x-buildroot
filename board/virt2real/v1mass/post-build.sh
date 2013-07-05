8c991bc712ee9a4c3eda7458fd7813afd684cae2
Gol <hypergol@gmail.com>
2013-07-05 05:34:09 +0400

    A little changes in additions

--- board/virt2real/v1mass/post-build.sh
+++ board/virt2real/v1mass/post-build.sh
@@ -5,8 +5,7 @@ BOARDDIR=board/virt2real/v1mass
 
 # Set root password to 'root'. Password generated with
 # mkpasswd, from the 'whois' package in Debian/Ubuntu.
-#sed -i 's%root::%root:8kfIfYHmcyQEE:%' $TARGETDIR/etc/shadow
+sed -i 's%root::%root:8kfIfYHmcyQEE:%' $TARGETDIR/etc/shadow
 
 # Copy the rootfs additions
-cp -a $BOARDDIR/rootfs-additions/* $TARGETDIR/
-
+sudo cp -r $BOARDDIR/rootfs-additions/* $TARGETDIR/
