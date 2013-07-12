a5a45f38e652adacd488faf11c08016faab56fd1
Gol <hypergol@gmail.com>
2013-07-13 01:06:22 +0400

    Fixed MC ncurses include and lib

--- package/mc/mc.mk
+++ package/mc/mc.mk
@@ -7,10 +7,13 @@ MC_VERSION = 4.8.8
 MC_SITE = http://ftp.midnight-commander.org/
 MC_SOURCE = mc-$(MC_VERSION).tar.bz2
 MC_DEPENDENCIES += ncurses libglib2
+#MC_DEPENDENCIES += slang libglib2
 MC_CONF_OPT += \
 	--with-screen=ncurses \
-	--with-ncurses-libs=$(STAGING_DIR)/usr/lib \
-	--with-ncurses-includes=$(STAGING_DIR)/usr/include \
+#	--with-ncurses-libs=$(STAGING_DIR)/usr/lib \
+#	--with-ncurses-includes=$(STAGING_DIR)/usr/include \
+#	--with-slang-libs=$(STAGING_DIR)/usr/lib \
+#	--with-slang-includes=$(STAGING_DIR)/usr/include \
 	--without-x \
 	--with-subshell \
 	--disable-rpath
